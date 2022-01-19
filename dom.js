c5af57c101c7575b5603c1cd9f839d6dd1f2ee2f
sadebell937 <93891161+sadebell937@users.noreply.github.com>
2022-01-19 18:16:23 -0500

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,35 @@
+function main ( ){
+// add the "big" class to the "grow me" paragraph
+const el = document.getElementById ("grow-me");
+p.classList.add("big");
+
+// remove the "big" class from the "shrink-me" paragraph
+const shrink = document.getElementById("shrink-me");
+p.classList.remove("big");
+
+// find all the <li>s and log their next context to the console.
+ document.querySelectorAll("li");
+ console.log("li");
+
+
+ // Set the href of the link to "https:www.example.com" and update the text to say
+ // "somewhere" instead of "nowhere".
+ a.setAttribute("href, http:www.example.com");
+ a.innerText = "nowhere";
+
+ // Set the "display" CSS property of the "hide-me" paragraph 
+ // to "none"
+ let styledis = document.getElementById ("hide-me");
+ styledis.style.display = "none";
+
+ //Set the display Css property of the "show-me" paragraph
+ // to "block"
+ let showme = document.getElementById("show-me");
+ showme.style.display = "block";
+
+ //Get the text that the user enters into the "name" input box
+ // and use it to set a welcome message in the <h1>, 
+ // "welcome Grant!"
+ el.setAttribute ("input", "<h1>");
+
+}
